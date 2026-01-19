1791dc8bcb6f648de6846fe9db3534843193baeb
Arpita Baraskar <arpitabaraskar22@gmail.com>
2026-01-19 10:07:25 +0530

    modified models.py

--- asiatouragency/models.py
+++ asiatouragency/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Tour(models.Model):
+    #we need a origin country, destination, number of nights, and we need the price for the tour.
+    origin_country = models.CharField(max_length=64)
+    destination_country = models.CharField(max_length=64)
+    number_of_nights = models.IntegerField()
+    price = models.IntegerField()
+
+ 
